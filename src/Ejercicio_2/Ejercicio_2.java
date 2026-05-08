e7ea1e2d91e5497cdd97e08c75dbd94aa878d1bb
Jhonatan-Cholca <deimos1996-rey@outlook.es>
2026-05-08 00:35:11 -0500

    Descripción del cambio

--- src/Ejercicio_2/Ejercicio_2.java
+++ src/Ejercicio_2/Ejercicio_2.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Ejercicio_2;
 
-/**
- *
- * @author L0rd De!m0z
- */
-public class NewClass {
-    
+public class Ejercicio_2 {
+    public static void main(String[] args) {
+        System.out.println("Test Jhonatan");
+    }
 }
